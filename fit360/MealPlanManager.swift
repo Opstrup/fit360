f2955ce4e4093b7424a0e0854b6ea6e42f95ab06
Anders Opstrup <aopstrup@gmail.com>
2016-12-18 22:10:04 +0100

    added mealPlanManager class and test file

---
+++ fit360/MealPlanManager.swift
@@ -0,0 +1,13 @@
+//
+//  MealPlanManager.swift
+//  fit360
+//
+//  Created by Anders Opstrup on 18/12/2016.
+//  Copyright © 2016 smallcity. All rights reserved.
+//
+
+import Foundation
+
+class MealPlanManager {
+    private var _desiredNumberOfMeals: NSNumber?
+}
---
+++ fit360Tests/mealPlanManagerTests.swift
@@ -0,0 +1,23 @@
+//
+//  mealPlanManagerTests.swift
+//  fit360
+//
+//  Created by Anders Opstrup on 18/12/2016.
+//  Copyright © 2016 smallcity. All rights reserved.
+//
+
+import XCTest
+
+class mealPlanManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
