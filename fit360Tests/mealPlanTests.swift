34b449dd111416f7777cce2addcb3e578fb9dcd6
Anders Opstrup <aopstrup@gmail.com>
2016-12-23 23:11:43 +0100

    Added test file for mealPlan

--- fit360Tests/mealPlanTests.swift
+++ fit360Tests/mealPlanTests.swift
@@ -1,14 +1,14 @@
 //
-//  mealPlanManagerTests.swift
+//  mealPlanTests.swift
 //  fit360
 //
-//  Created by Anders Opstrup on 18/12/2016.
+//  Created by Anders Opstrup on 23/12/2016.
 //  Copyright © 2016 smallcity. All rights reserved.
 //
 
 import XCTest
 
-class mealPlanManagerTests: XCTestCase {
+class mealPlanTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -18,5 +18,5 @@ class mealPlanManagerTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
+    }    
 }
